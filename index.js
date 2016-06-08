b4fb7535490e748e5977cfce67967f44a1817f9b
Mark Stratmann <mark@stratmann.me.uk>
2016-06-08 11:01:51 +0100

    Enable function to accept environment variable overrides.

--- index.js
+++ index.js
@@ -21,23 +21,65 @@ exports.handler = function(event, context) {
 
   var ecs = new aws.ECS({ region: region });
 
+  var env_vars = {};
+  if (event.environment) env_vars = event.environment;
+
   var promise = when.promise(function(resolve, reject, notify) {
 
-    var params = {
+    var task_params = {
       taskDefinition: event.task, /* required */
       cluster: event.cluster,
       count: count,
       startedBy: started_by
     };
 
-    ecs.runTask(params, function(err, data) {
-      if (err) {
-        reject(err);
+    // Get the container definition details
+    when.promise(function(resolve, reject, notify) {
+      if (_.keys(env_vars).length > 0) {
+        var def_params = {
+          taskDefinition: event.task
+        };
+        ecs.describeTaskDefinition(def_params, function(err, data) {
+          if (err) {
+            reject(err);
+          }
+          else {
+            var container_overrides = _.map(data.taskDefinition.containerDefinitions, function(container){
+              return {
+                name: container.name,
+                environment: _.map(_.keys(env_vars), function(key){
+                  return {
+                    name: key,
+                    value: env_vars[key]
+                  };
+                })
+              };
+            });
+            // Set up the container override parameters
+            task_params.overrides = {};
+            task_params.overrides.containerOverrides = container_overrides;
+            resolve();
+          }
+        });
       } else {
-        console.log(data);
-        resolve(data);
+        resolve();
       }
+    }).done(function(){
+      console.log('task_params');
+      console.log(JSON.stringify(task_params));
+      // Start the ECS Task
+      ecs.runTask(task_params, function(err, data) {
+        if (err) {
+          reject(err);
+        } else {
+          console.log(data);
+          resolve(data);
+        }
+      });
+    }, function(err){
+      reject(err);
     });
+
 
   });
 
